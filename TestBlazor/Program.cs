86b84bd8be149aad909ca567daf9228df7e22972
test-007-jira-merely <beezi.merely@gmail.com>
2026-04-14 11:24:27 +0300

    Update Program.cs

--- TestBlazor/Program.cs
+++ TestBlazor/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TestBlazor;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builde = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
